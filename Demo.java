29ab3fd783d9c7bda526a5a724f5b21efa3a396e
Amar <amarjadhavoffice10@gmail.com>
2026-02-15 19:14:07 +0530

    git stash practise

--- Demo.java
+++ Demo.java
@@ -3,4 +3,8 @@ public class Demo
     int a=0;
 // to unstage a file from staging area
 // restore cmd is used to discard the changes and also to unsatge a file
+
+// jira-102
+ 	mnbv	
+	jgfhd	
   }
